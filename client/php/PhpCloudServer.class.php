edc1078d30764ba77903a48b3f085e873585f82e
soywiz <soywiz@gmail.com>
2012-09-22 02:23:47 +0200

    Upload files

--- client/php/PhpCloudServer.class.php
+++ client/php/PhpCloudServer.class.php
@@ -13,6 +13,45 @@ class PhpCloudServer {
 		return $this->requestJson('tree.get');
 	}
 	
+	public function uploadAllNonUploadedFiles($folder) {
+		$local = $this->getAllFolderFilesRecursively($folder);
+		$online = $this->getAllOnlineFiles();
+		$pendingList = array_diff($local, $online);
+		foreach ($pendingList as $pending) {
+			if (is_dir("{$folder}/{$pending}")) {
+				
+			} else {
+				$this->uploadFile($pending, $folder, $pending);
+			}
+		}
+	}
+	
+	protected function getAllFolderFilesRecursively($folder) {
+		$paths = array();
+		foreach (
+			new RecursiveIteratorIterator(new RecursiveDirectoryIterator($folder), RecursiveIteratorIterator::CHILD_FIRST)
+			as $path
+		) {
+			/* @var $path SplFileInfo */
+			$paths[] = trim(str_replace('\\', '/', substr($path->getPathname(), strlen($folder))), '/');
+		}
+		return $paths;
+	}
+	
+	protected function getAllOnlineFiles() {
+		$paths = array();
+		foreach ($this->getAllFiles() as $file) $paths[] = $file->path;
+		return $paths;
+	}
+	
+	public function downloadAllFilesTo($folder) {
+		foreach ($this->getAllFiles() as $file) {
+			echo "{$file->path}...";
+			$this->downloadFile($file, $folder);
+			echo "Ok\n";
+		}
+	}
+	
 	protected function getUrl($action, $params = array()) {
 		return $this->url . '/?' . http_build_query(array('action' => $action) + $params);
 	}
@@ -31,14 +70,49 @@ class PhpCloudServer {
 		return implode('/', $components);
 	}
 	
-	public function downloadFile($remoteFile, $localPath, $localFile = NULL) {
+	static protected function combinePath($path1, $path2) {
+		$path1 = static::normalizePath($path1) . '/';
+		$path2 = static::normalizePath($path1 . '/' . $path2);
+		
+		if (strpos($path2, $path1) !== 0) throw(new Exception("Invalid path"));
+		
+		return $path2;
+	}
+
+	public function uploadFile($remoteFile, $localPath, $localFile = NULL) {
 		if ($localFile === NULL) $localFile = $remoteFile; 
 		
-		$localPath = static::normalizePath($localPath) . '/';
-		$realLocalFile = static::normalizePath($localPath . '/' . $localFile);
+		$realLocalFile = static::combinePath($localPath, $localFile);
 		
-		if (strpos($realLocalFile, $localPath) !== 0) throw(new Exception("Invalid path"));
+		$sha1 = sha1_file($realLocalFile);
 		
+		if (!$this->requestJson('file.has', array('sha1' => $sha1))) {
+			$ch = curl_init();
+			curl_setopt($ch, CURLOPT_VERBOSE, 0);
+			curl_setopt($ch, CURLOPT_URL, $this->getUrl('file.upload'));
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+			curl_setopt($ch, CURLOPT_USERPWD, "{$this->username}:{$this->password}");
+			curl_setopt($ch, CURLOPT_POSTFIELDS, array(
+				'file' => "@{$realLocalFile}",
+			));
+			$response = $this->parseJson(curl_exec($ch));
+			curl_close($ch);
+		}
+		
+		$info = $this->requestJson('tree.add', array(
+			'path' => $remoteFile,
+			'sha1' => $sha1,
+			'ctime' => filectime($realLocalFile),
+			'mtime' => filemtime($realLocalFile),
+			'perms' => '0777',
+		));
+	}
+	
+	public function downloadFile($remoteFileInfo, $localPath, $localFile = NULL) {
+		$remoteFile = $remoteFileInfo->path;
+		if ($localFile === NULL) $localFile = $remoteFile; 
+		
+		$realLocalFile = static::combinePath($localPath, $localFile);
 		$realLocalPath = dirname($realLocalFile);
 		
 		if (!is_dir($realLocalPath)) {
@@ -47,6 +121,7 @@ class PhpCloudServer {
 		
 		if (!is_file($realLocalFile)) {
 			copy($this->getUrl('tree.file.get', array('path' => $remoteFile)), $realLocalFile, $this->getContext());
+			touch($realLocalFile, $remoteFileInfo->mtime);
 		}
 	}
 	
@@ -62,9 +137,13 @@ class PhpCloudServer {
 		return file_get_contents($this->getUrl($action, $params), false, $this->getContext());
 	}
 	
-	public function requestJson($action, $params = array()) {
-		$info = json_decode($this->request($action, $params));
+	private function parseJson($jsonData) {
+		$info = json_decode($jsonData);
 		if ($info->result != 'ok') throw(new Exception("Error {$info->result} : {$info->data}"));
 		return $info->data;
+	}
+	
+	public function requestJson($action, $params = array()) {
+		return $this->parseJson($this->request($action, $params));
 	}
 }
--- client/php/sync.php
+++ client/php/sync.php
@@ -5,8 +5,5 @@ require_once(__DIR__ . '/PhpCloudServer.class.php');
 $config = (object)require(__DIR__ . '/config.inc.php');
 
 $server = new PhpCloudServer($config->url, $config->username, $config->password);
-foreach ($server->getAllFiles() as $file) {
-	echo "{$file->path}...";
-	$server->downloadFile($file->path, $config->output_folder);
-	echo "Ok\n"; 
-}
+$server->downloadAllFilesTo($config->output_folder);
+$server->uploadAllNonUploadedFiles($config->output_folder);
